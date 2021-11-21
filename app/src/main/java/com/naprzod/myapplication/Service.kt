b7b0f0477470424798a4c2a62d6ff2872dd4e61c
Miguel <mdiluca@itba.edu.ar>
2021-11-21 00:00:46 +0000

    Initial commit

---
+++ app/src/main/java/com/naprzod/myapplication/Service.kt
@@ -0,0 +1,14 @@
+package com.naprzod.myapplication;
+
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+
+class Service : AccessibilityService(){
+    override fun onAccessibilityEvent(p0: AccessibilityEvent?) {
+
+    }
+
+    override fun onInterrupt() {
+
+    }
+}
