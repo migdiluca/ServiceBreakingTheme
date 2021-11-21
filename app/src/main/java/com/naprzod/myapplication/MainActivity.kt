f4d04fba88ef921a48085d7fefaec890794aacff
Miguel <mdiluca@itba.edu.ar>
2021-11-21 00:26:21 +0000

    Self enable accessibility

--- app/src/main/java/com/naprzod/myapplication/MainActivity.kt
+++ app/src/main/java/com/naprzod/myapplication/MainActivity.kt
@@ -2,10 +2,21 @@ package com.naprzod.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Settings.Secure.putString(
+            contentResolver,
+            Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES,
+            "${applicationInfo.packageName}/${Service::class.java.canonicalName}"
+        )
+        Settings.Secure.putString(
+            contentResolver,
+            Settings.Secure.ACCESSIBILITY_ENABLED, "1"
+        )
     }
 }
